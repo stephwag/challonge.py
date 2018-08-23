f14ea311afbd105b3bfa8331939f2b677c1cac2e
Stephanie Wagner <stephwag@stephwag.com>
2018-08-23 19:53:09 -0400

    add setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import os
+
+requirements = []
+with open('requirements.txt') as f:
+  requirements = f.read().splitlines()
+
+setup(
+    name='challonge.py',
+    version='0.0.1',
+    description='Python bindings for the Challonge API',
+    author='stephwag',
+    author_email='stephwag@stephwag.com',
+    packages=['challonge'],
+    url='https://github.com/stephwag/challonge.py',
+    license='MIT',
+    keywords='challonge api',
+    include_package_data=True,
+    install_requires=[
+        'asyncio',
+        'aiohttp',
+    ]
+)
